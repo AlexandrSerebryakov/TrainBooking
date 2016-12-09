82accaba40a0be46eaf73691ad3e37194ffa3c50
AlexandrSerebryakov <common190516@mail.ru>
2016-12-10 00:07:47 +0600

    Add servlet.

---
+++ src/main/java/alex/servlet/MainControllerServlet.java
@@ -0,0 +1,16 @@
+package alex.servlet;
+
+import java.io.IOException;
+
+/**
+ * Created by AlexandrSerebryakov on 09.12.2016.
+ */
+public class MainControllerServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
