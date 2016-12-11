746052566388e93a1b80d875bee8c7378a27588d
AlexandrSerebryakov <common190516@mail.ru>
2016-12-11 22:59:10 +0600

    Add jsp, do'nt debug.

--- src/main/java/alex/servlet/MainControllerServlet.java
+++ src/main/java/alex/servlet/MainControllerServlet.java
@@ -1,11 +1,19 @@
 package alex.servlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
  * Created by AlexandrSerebryakov on 09.12.2016.
  */
 public class MainControllerServlet extends javax.servlet.http.HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: user
+  Date: 10.12.2016
+  Time: 21:23
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<a href="${pageContext.request.contextPath}do/user/login">Registration</a>
+
+<form action="do/user/login" method="post">
+    <input type="hidden" name="action" value="register">
+    <button type="submit">SUBMIT</button>
+</form>
+
+</body>
+</html>
