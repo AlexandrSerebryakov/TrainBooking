86c4085a503924bc618d90c71d2b8b68e371549e
AlexandrSerebryakov <common190516@mail.ru>
2016-12-12 17:32:32 +0600

    Debug jsp.

--- src/main/java/alex/servlet/MainControllerServlet.java
+++ src/main/java/alex/servlet/MainControllerServlet.java
@@ -1,6 +1,8 @@
 package alex.servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -8,17 +10,18 @@ import java.io.IOException;
 /**
  * Created by AlexandrSerebryakov on 09.12.2016.
  */
-public class MainControllerServlet extends javax.servlet.http.HttpServlet {
+@WebServlet(name = "MainControllerServlet")
+public class MainControllerServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.service(req, resp);
     }
 
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    /*protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
-    }
+    }*/
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -6,7 +6,7 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
 <head>
     <title>Title</title>
@@ -15,8 +15,7 @@
 <a href="${pageContext.request.contextPath}do/user/login">Registration</a>
 
 <form action="do/user/login" method="post">
-    <input type="hidden" name="action" value="register">
-    <button type="submit">SUBMIT</button>
+
 </form>
 
 </body>
