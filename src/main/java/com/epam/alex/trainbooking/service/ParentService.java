41640cf368c15e704cb92c6919adfe45caeddea6
AlexandrSerebryakov <common190516@mail.ru>
2016-12-18 19:21:03 +0600

    Build hierarchy.

--- src/main/java/com/epam/alex/trainbooking/service/ParentService.java
+++ src/main/java/com/epam/alex/trainbooking/service/ParentService.java
@@ -3,5 +3,5 @@ package com.epam.alex.trainbooking.service;
 /**
  * Created by ${AlexandrSerebryakov} on ${09.10.2016}.
  */
-public class Selection {
+public abstract class ParentService {
 }
---
+++ src/main/java/com/epam/alex/trainbooking/validator/RegExprValidator.java
@@ -0,0 +1,7 @@
+package com.epam.alex.trainbooking.validator;
+
+/**
+ * Created by ${AlexandrSerebryakov} on ${09.10.2016}.
+ */
+public class RegExprValidator extends ParentValidator implements Validator {
+}
