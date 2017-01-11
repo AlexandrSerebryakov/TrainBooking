7bbb6d0a23dcc57bc873a0e71cea4135bd9d78f5
AlexandrSerebryakov <common190516@mail.ru>
2017-01-11 19:11:23 +0600

    Add class "ConnectionPool", edit class "PropertyManager".

--- src/main/java/com/epam/alex/trainbooking/util/PropertyManager.java
+++ src/main/java/com/epam/alex/trainbooking/util/PropertyManager.java
@@ -1,23 +1,39 @@
 package com.epam.alex.trainbooking.util;
 
+
+import com.epam.alex.trainbooking.exception.PropertyManagerException;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 /**
- * Created by ${AlexandrSerebryakov} on ${09.10.2016}.
+ * Property manager for serve reading properties from file
  */
+
 public class PropertyManager {
+
+    private static final String PROPERTY_NOT_LOADED_ERROR_MESSAGE = "Properties not loaded.";
     private Properties properties;
-    public PropertyManager(String propertyFileName) throws IOException {
+
+    public PropertyManager(String propertyFileName) throws PropertyManagerException {
+
         properties = new Properties();
-        InputStream in = PropertyManager.class.getClassLoader().getResourceAsStream(propertyFileName);
+        try (InputStream in = PropertyManager.class.getClassLoader().getResourceAsStream(propertyFileName)) {
             properties.load(in);
+        } catch (IOException e) {
+            throw new PropertyManagerException(e);
         }
+    }
+
+    public String getPropertyKey(String key) throws PropertyManagerException {
+
+        if (properties == null) throw new PropertyManagerException(PROPERTY_NOT_LOADED_ERROR_MESSAGE);
+        return properties.getProperty(key);
+    }
+
     public Properties getProperties() {
         return properties;
     }
 
 }
-
-
